308abf2fd9abfae98e39011556e6d40a4f859859
prathithkumar <prathithkumarkjk@gmail.com>
2023-09-20 17:46:51 +0200

    practice shell

--- 03-sample-vm/vm.tf
+++ 03-sample-vm/vm.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "app" {
   ami           = "ami-0f5dbf035a6aca15c"
-  instance_type = "t3.micro"
+  instance_type = "t3.medium"
 
   tags = {
     Name = "My-Terraform-Instance"
