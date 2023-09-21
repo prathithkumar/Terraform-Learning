b8e45f17c1757a383944b2f36dbb2bb9463bd4c6
prathithkumar <prathithkumarkjk@gmail.com>
2023-09-21 20:11:33 +0200

    practice shell

--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -1,11 +1,11 @@
 # Creating EC2 
-resource "aws_instance" "app" {
+resource "aws_instance" "moapp" {
   ami                       = "ami-0f5dbf035a6aca15c"
   instance_type             = "t3.medium"
   vpc_security_group_ids    = [var.sg]
 
     tags = {
-    Name           = "Terraform-Instance"
+    Name           = "Module-Instance"
     }
 }
 
